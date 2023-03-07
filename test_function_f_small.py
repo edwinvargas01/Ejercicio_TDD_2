1ddf7ef38f834b6178cc6747938d266872c3994f
Ubuntu <ubuntu@ip-172-31-76-210.ec2.internal>
2023-03-07 03:09:37 +0000

    Delete some files that  I used in the several tests

--- test_function_f_small.py
+++ test_function_f_small.py
@@ -5,5 +5,5 @@ def test_is_smallest():
     assert find_Num([5,7,4,3.8])==3.8;
     assert find_Num([10,math.sqrt(16),math.pow(2,6),math.factorial(9)])==4.0;
     assert find_Num([abs(-18),-23,-15,0])==-23;
-    assert find_Num([7,1,0,2])==0;
+    assert find_Num([7,1,0,3])==0;
     
